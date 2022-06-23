260d43753baff237490fe7353cbb0b367ba24548
Joe Creager <josephcreager@gmail.com>
2022-06-23 16:51:48 -0700

    add cert manager module

---
+++ certmanager/main.tf
@@ -0,0 +1,24 @@
+provider "helm" {
+  kubernetes {
+    host                   = var.k8s_host
+    client_certificate     = base64decode(var.client_certificate)
+    client_key             = base64decode(var.client_key)
+    cluster_ca_certificate = base64decode(var.cluster_ca_certificate
+  }
+}
+
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/helm"
+      version = "~>2.6"
+    }
+  }
+}
+
+resource "helm_release" "certmanager" {
+  name = var.site_name
+  repository = "https://charts.jetstack.io"
+  chart = "jetstack/cert-manager"
+  create_namespace = true
+}
