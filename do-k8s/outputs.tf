1b915198bd2ac30dc0c222b427957381135d178d
Joe Creager <josephcreager@gmail.com>
2022-06-23 18:05:05 -0700

    Update outputs.tf

--- do-k8s/outputs.tf
+++ do-k8s/outputs.tf
@@ -1,7 +1,7 @@
 output "k8s_host" {
-  value = digital_ocean_kubernetes_cluster.do_k8s.endoint
+  value = digitalocean_kubernetes_cluster.do_k8s.endoint
 }
 
 output "kube_config" {
- value = digital_ocean_kubernetes_cluster.do_k8s.kube_config[0]
+ value = digitalocean_kubernetes_cluster.do_k8s.kube_config[0]
 }
