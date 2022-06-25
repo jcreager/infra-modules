39d82d1e9d320f43b281571b91e54c67a3991270
Joe Creager <josephcreager@gmail.com>
2022-06-25 16:35:29 -0700

    add token auth

--- ingress-nginx/main.tf
+++ ingress-nginx/main.tf
@@ -1,8 +1,9 @@
 provider "helm" {
   kubernetes {
     host                   = var.k8s_host
-    client_certificate     = base64decode(var.client_certificate)
-    client_key             = base64decode(var.client_key)
+    #client_certificate     = base64decode(var.client_certificate)
+    #client_key             = base64decode(var.client_key)
+    token                  = base64decode(var.k8s_token)
     cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
   }
 }
@@ -17,7 +18,7 @@ terraform {
 }
 
 resource "helm_release" "ingress-nginx" {
-  name = "ingress-nginx" 
+  name = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart = "ingress-nginx"
   create_namespace = true
--- ingress-nginx/variables.tf
+++ ingress-nginx/variables.tf
@@ -1,4 +1,5 @@
 variable "k8s_host" {}
+variabel "k8s_token" {}
 variable "client_certificate" {}
 variable "client_key" {}
 variable "cluster_ca_certificate" {}
