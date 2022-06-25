6667e7186f1888e4c9171089d9a4095f9b5fd979
Joe Creager <josephcreager@gmail.com>
2022-06-25 16:38:29 -0700

    Update variables.tf

--- ingress-nginx/variables.tf
+++ ingress-nginx/variables.tf
@@ -1,5 +1,5 @@
 variable "k8s_host" {}
 variable "k8s_token" {}
-variable "client_certificate" {}
-variable "client_key" {}
+#variable "client_certificate" {}
+#variable "client_key" {}
 variable "cluster_ca_certificate" {}
