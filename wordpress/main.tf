9d66671c2af5f47ffb2d230b09b4f414bb809445
Joe Creager <josephcreager@gmail.com>
2022-06-26 14:10:38 -0700

    update wp deployment

--- wordpress/main.tf
+++ wordpress/main.tf
@@ -4,12 +4,40 @@ provider "helm" {
   }
 }
 
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+
 terraform {
   required_providers {
-    kubernetes = {
+    helm = {
       source = "hashicorp/helm"
       version = "~>2.6"
     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~>2.11"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~>3.3"
+    }
+  }
+}
+
+
+resource "random_password" "wordpress_password" {
+  length = 32
+}
+
+resource "kubernetes_secret" "wordpress" {
+  metadata {
+    name = "${var.site-name}-secrets"
+  }
+  data = {
+    wp_username = "admin"
+    wp_password = random_password.wordpress_password.result
   }
 }
 
@@ -18,4 +46,41 @@ resource "helm_release" "wordpress" {
   repository = "https://charts.bitnami.com/bitnami"
   chart = "wordpress"
   create_namespace = true
+  set {
+    name = "wordpressUsername"
+    value_from {
+      secret_key_ref {
+        name = kubernetes_secret.wordpress.metadata.name
+        key = "wp_username"
+      }
+    }
+  set {
+    name = "wordpressPassword"
+    value_from {
+      secret_key_ref {
+        name = kubernetes_secret.wordpress.metadata.name
+        key = "wp_password"
+      }
+    }
+  }
+  set {
+    name = "wordpressConfigureCache"
+    value = true
+  }
+  set {
+    name = "ingress.enabled"
+    value = true
+  }
+  set {
+    name = "ingress.hostname"
+    value = var.site_hostname
+  }
+  set {
+    name = "ingress.extraHosts"
+    value = var.site_extra_hostnames
+  }
+  set {
+    name = "ingress.tls"
+    value = true
+  }
 }
--- wordpress/variables.tf
+++ wordpress/variables.tf
@@ -1,1 +1,5 @@
 variable "site_name" {}
+variable "site_hostname" {}
+variable "site_extra_hostnames" {
+  default = []
+}
