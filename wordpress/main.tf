f31e754cafc7e04fcc1447c9520e8a7b23e42631
Joe Creager <josephcreager@gmail.com>
2022-06-26 17:32:19 -0700

    Update main.tf

--- wordpress/main.tf
+++ wordpress/main.tf
@@ -30,11 +30,59 @@ resource "random_password" "wordpress_password" {
   length = 32
 }
 
+resource "kubernetes_manifest" "configmap_wordpress" {
+  manifest = {
+    "apiVersion" = "v1"
+    "data" = {
+      "uploads.ini" = <<-EOT
+      ; Maximum allowed size for uploaded files.
+      upload_max_filesize = 1024M
+
+      ; Must be greater than or equal to upload_max_filesize
+      post_max_size = 1024M
+      max_execution_time = 500
+      EOT
+    }
+    "kind" = "ConfigMap"
+    "metadata" = {
+      "labels" = {
+        "app" = "wordpress"
+      }
+      "name" = "wordpress"
+      "namespace" = "default"
+    }
+  }
+}
+
 resource "helm_release" "wordpress" {
   name = var.site_name
   repository = "https://charts.bitnami.com/bitnami"
   chart = "wordpress"
   create_namespace = true
+  values = [
+    yamlencode(
+      {
+        "extraVolumeMounts" = [
+          {
+            name = "php-ini"
+            "mountPath" = "/opt/bitnami/php/etc/conf.d"
+          }
+        ]
+      }
+    ),
+    yamlencode(
+      {
+        "extraVolumes" = [
+          {
+            name = "php-ini"
+            "configMap" = {
+              name = "php-ini"
+            }
+          }
+        ]
+      }
+    )
+  ]
   set {
     name = "wordpressUsername"
     value = "admin"
@@ -61,6 +109,7 @@ resource "kubernetes_ingress_v1" "wordpress" {
       "kubernetes.io/ingress.global-static-ip-name" = "167.172.8.60"
       "kubernetes.io/ingress.class" = "nginx"
       "nginx.ingress.kubernetes.io/proxy-body-size" = "1024m"
+      "nginx.ingress.kubernetes.io/client-max-body-size" = "1024m"
       "cert-manager.io/cluster-issuer" = "letsencrypt-prod"
     }
   }
