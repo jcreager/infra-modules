062cbb56e7173d8b7e98303b40c69edb2d973923
Joe Creager <josephcreager@gmail.com>
2022-06-27 12:59:01 -0700

    Update main.tf

--- ingress-nginx/main.tf
+++ ingress-nginx/main.tf
@@ -18,4 +18,8 @@ resource "helm_release" "ingress-nginx" {
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart = "ingress-nginx"
   create_namespace = true
+  set {
+    name = "controller.service.externalTrafficPolicy"
+    value = "Local"
+  }
 }
