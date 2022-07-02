3dd6941118855408e3a96b75224aa8281506e78e
Joe Creager <josephcreager@gmail.com>
2022-07-02 16:33:47 -0700

    Update main.tf

--- blackbox-exporter/main.tf
+++ blackbox-exporter/main.tf
@@ -2,6 +2,22 @@ provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
   }
+  provider "kubernetes" {
+    config_path = "~/.kube/config"
+  }
+}
+
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+      version = "~>2.6"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~>2.11.0"
+    }
+  }
 }
 
 resource "helm_release" "certmanager" {
