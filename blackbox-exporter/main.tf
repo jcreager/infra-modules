6dd37fc9c32121dbcd97223e9c0e82d483cf6c83
Joe Creager <josephcreager@gmail.com>
2022-07-02 16:37:16 -0700

    Update main.tf

--- blackbox-exporter/main.tf
+++ blackbox-exporter/main.tf
@@ -2,9 +2,10 @@ provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
   }
-  provider "kubernetes" {
-    config_path = "~/.kube/config"
-  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
 }
 
 terraform {
