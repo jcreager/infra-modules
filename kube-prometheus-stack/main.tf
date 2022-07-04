3daff7850ee13e08cd882ce37c128eef85e5a25f
Joe Creager <josephcreager@gmail.com>
2022-07-04 12:08:57 -0700

    Update main.tf

--- kube-prometheus-stack/main.tf
+++ kube-prometheus-stack/main.tf
@@ -13,53 +13,33 @@ terraform {
   }
 }
 
+resource "kubernetes_secret" "additional_scrape_configs" {
+  metadata {
+    name = "additional-scrape-configs"
+  }
+  data = {
+    "additional-scrape-configs.yaml" = var.scrape_configs
+  }
+  type = "Opaque"
+}
+
 resource "helm_release" "kube-prometheus-stack" {
   name = "kube-prometheus-stack"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart = "kube-prometheus-stack"
   create_namespace = true
   skip_crds = true
-  values = [
-    yamlencode(
-  #    {
-  #      #  "ingress.enabled" = true
-  #      #  "hosts" = [var.grafana_host]
-  #      #  "tls" = [
-  #      #    {
-  #      #      "secretName" = var.tls_secret_name
-  #      #      "hosts" = [var.grafana_host]
-  #      #    }
-  #      #  ]
-  {"extraScrapeConfigs" = var.scrape_configs}
-  #      "extraScrapeConfigs" = [
-  #        {
-  #          "job_name" = "blackbox"
-  #          "metrics_path" = "/probe"
-  #          "params" = {
-  #            "module" = ["http_2xx"]
-  #          }
-  #          "static_configs" = [{"targets" = var.scrape_targets}]
-  #          "relable_configs" = [
-  #            {
-  #              "source_labels" = ["__address__"]
-  #              "target_labels" = "__param_target"
-  #            },
-  #            {
-  #              "source_labels" = ["__param_target"]
-  #              "target_labels" = "instance"
-  #            },
-  #            {
-  #              "source_labels" = ["__address__"]
-  #              "target_labels" = "blackbox-exporter-prometheus-blackbox-exporter.default.svc.cluster.local:9115"
-  #            }
-  #          ]
-  #        }
-  #      ]
-  #    }
-    )
-  ]
-  #set {
-  #  name = "extraScrapeConfigs.0"
-  #  value = var.scrape_configs
-  #}
+    set {
+    name  = "prometheus.prometheusSpec.additionalScrapeConfigsSecret.enabled"
+    value = true
+  }
+  set {
+    name  = "prometheus.prometheusSpec.additionalScrapeConfigsSecret.name"
+    value = "additional-scrape-configs"
+  }
+  set {
+    name  = "prometheus.prometheusSpec.additionalScrapeConfigsSecret.key"
+    value = "additional-scrape-configs.yaml"
+  }
 }
+
