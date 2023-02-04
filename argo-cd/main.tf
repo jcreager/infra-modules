ad8267bf2bce250c272eb0b98a840c3ced76e4c9
Joe Creager <josephcreager@gmail.com>
2023-02-04 13:27:00 -0800

    Add repo_creds secret

--- argo-cd/main.tf
+++ argo-cd/main.tf
@@ -21,10 +21,26 @@ terraform {
   }
 }
 
+resource "kubernetes_secret" "repo_creds" {
+  metadata {
+    name = "repo-credentials"
+    namespace = "argocd"
+    labels = {
+      "argocd.argoproj.io/secret-type" = "repo-creds"
+    }
+    string_data = {
+      "url" = var.repo_url
+      "type" = "helm"
+      "sshPrivateKey" = var.ssh_private_key
+    }
+  }
+}
+
 resource "helm_release" "agro-cd" {
   name = "agro-cd"
   repository = "https://argoproj.github.io/argo-helm"
   chart = "argo-cd"
   create_namespace = true
   skip_crds = true
+  # TODO Set version.
 }
